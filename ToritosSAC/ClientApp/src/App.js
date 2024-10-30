5f77eac356293db5675d7b3ee49fa07e2bba7012
ACER <ACER@LAPTOP-IPO1QDO0>
2024-10-29 20:01:26 -0500

    Initial commit

---
+++ ToritosSAC/ClientApp/src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+    return (
+        <div className="Container">
+            <h1>Hola a todos</h1>
+        </div>
+    )
+}
+
+export default App;
