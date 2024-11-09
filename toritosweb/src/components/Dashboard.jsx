9f7cf69ffea18b45c71a5d5f3bafc1350877d23d
ACER <ACER@LAPTOP-IPO1QDO0>
2024-11-08 23:39:39 -0500

    Eliminar submódulo toritosweb y agregar como parte del repositorio principal

---
+++ toritosweb/src/components/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Dashboard extends React.Component{
+
+    render(){
+        return(
+            <div>
+                Hola desde Dashboard
+            </div>
+        );
+    }
+}
+
+export default Dashboard
---
+++ toritosweb/src/services/apirest.js
@@ -0,0 +1,1 @@
+export const Apiurl = "http://localhost:5158/api/";
