a771f7f70e61e3f0f74ca7e2b51d65961334ced1
ACER <ACER@LAPTOP-IPO1QDO0>
2024-12-06 18:15:51 -0500

    Servicios Grupo V1

---
+++ ToritosSAC.Server/ToritosSAC.Entities/Structures/Resultado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToritosSAC.Entities.Structures
+{
+    public class Resultado<T>
+    {
+        public T Objeto { get; set; }
+        public string Mensaje { get; set; }
+        public bool Exito { get; set; }
+
+        public Resultado(T objeto, string mensaje, bool exito)
+        {
+            Objeto = objeto;
+            Mensaje = mensaje;
+            Exito = exito;
+        }
+    }
+}
